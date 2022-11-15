f23ffaa07773759555a5ae697e08e59ac6224fee
Kolodii Kvitoslava <92575017+kvitK1@users.noreply.github.com>
2022-11-15 17:32:32 +0200

    Update FlowerContinueApplicationTests.java

--- src/test/java/flower/flowercontinue/FlowerContinueApplicationTests.java
+++ src/test/java/flower/flowercontinue/FlowerContinueApplicationTests.java
@@ -1,14 +1,14 @@
-package flower.flowercontinue;
+// package flower.flowercontinue;
 
-import flower.flowercontinue.flower.FlowerContinueApplication;
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+// import flower.flowercontinue.flower.FlowerContinueApplication;
+// import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(classes=FlowerContinueApplication.class)
-class FlowerContinueApplicationTests {
+// @SpringBootTest(classes=FlowerContinueApplication.class)
+// class FlowerContinueApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+// 	@Test
+// 	void contextLoads() {
+// 	}
 
-}
+// }
