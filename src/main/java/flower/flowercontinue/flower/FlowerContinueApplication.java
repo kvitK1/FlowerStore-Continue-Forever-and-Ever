9393dd535e999d8faf848602cc92d4209d73f5ad
Kolodii Kvitoslava <92575017+kvitK1@users.noreply.github.com>
2022-11-15 16:18:43 +0200

    Update FlowerContinueApplication.java

--- src/main/java/flower/flowercontinue/flower/FlowerContinueApplication.java
+++ src/main/java/flower/flowercontinue/flower/FlowerContinueApplication.java
@@ -1,14 +1,14 @@
-package flower.flowercontinue.flower;
+// package flower.flowercontinue.flower;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+// import org.springframework.boot.SpringApplication;
+// import org.springframework.boot.autoconfigure.SpringBootApplication;
+// import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
-public class FlowerContinueApplication {
+// @SpringBootApplication
+// public class FlowerContinueApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FlowerContinueApplication.class, args);
-	}
+// 	public static void main(String[] args) {
+// 		SpringApplication.run(FlowerContinueApplication.class, args);
+// 	}
 
-}
+// }
